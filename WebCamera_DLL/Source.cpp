68486740618d335bf6ed306e26bca534668c66d7
kurepasu0731 <kurepasu0731@gmail.com>
2016-05-22 12:14:53 +0900

    修正(x反転色反転なくす)

--- WebCamera_DLL/Source.cpp
+++ WebCamera_DLL/Source.cpp
@@ -3,7 +3,7 @@
 DLLExport void* getCamera(int device)
 {
     // ウィンドウを開く
-    cv::namedWindow("hoge");
+    cv::namedWindow("web camera");
     return static_cast<void*>(new cv::VideoCapture(device));
 }
 
@@ -19,7 +19,7 @@ DLLExport void setCameraProp(void* camera, int width, int height, int fps)
 DLLExport void releaseCamera(void* camera)
 {
     // ウィンドウを閉じる
-    cv::destroyWindow("hoge");
+    cv::destroyWindow("web camera");
     auto vc = static_cast<cv::VideoCapture*>(camera);
     delete vc;
 }
@@ -35,22 +35,24 @@ DLLExport void getCameraTexture(void* camera, unsigned char* data)
 	//std::cout << "img size:" << img.rows << " * " << img.cols << std::endl;
     
     // リサイズ
-    //cv::Mat resized_img(height, width, img.type());
-    //cv::resize(img, resized_img, resized_img.size(), cv::INTER_CUBIC);
+	cv::Mat resized_img(img.rows * 0.2, img.cols * 0.2, img.type());
+    cv::resize(img, resized_img, resized_img.size(), cv::INTER_CUBIC);
     
     // 別ウィンドウの画を更新
-    //cv::imshow("hoge", resized_img);
-    cv::imshow("hoge", img);
+    cv::imshow("web camera", resized_img);
     
+	//↓↓処理するのはこっちに戻ってやるのだからx反転色変換いらないのでは？？
     // RGB --> ARGB 変換
-    cv::Mat argb_img, flip_img;
-    //cv::cvtColor(resized_img, argb_img, CV_RGB2BGRA);
-    cv::cvtColor(img, argb_img, CV_RGB2BGRA);
-    std::vector<cv::Mat> bgra;
-    cv::split(argb_img, bgra);
-    std::swap(bgra[0], bgra[3]);
-    std::swap(bgra[1], bgra[2]);
-	//x軸反転
-	cv::flip(argb_img, flip_img, 0);
-    std::memcpy(data, flip_img.data, flip_img.total() * flip_img.elemSize());
+ //   cv::Mat argb_img, flip_img;
+ //   //cv::cvtColor(resized_img, argb_img, CV_RGB2BGRA);
+ //   cv::cvtColor(img, argb_img, CV_RGB2BGRA);
+ //   std::vector<cv::Mat> bgra;
+ //   cv::split(argb_img, bgra);
+ //   std::swap(bgra[0], bgra[3]);
+ //   std::swap(bgra[1], bgra[2]);
+	////x軸反転
+	//cv::flip(argb_img, flip_img, 0);
+ //   std::memcpy(data, flip_img.data, flip_img.total() * flip_img.elemSize());
+
+    std::memcpy(data, img.data, img.total() * img.elemSize());
 }
