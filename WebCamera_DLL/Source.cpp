0ff4cac5035422cd2664dcfd1665ab91b6664cc8
kurepasu0731 <kurepasu0731@gmail.com>
2016-09-23 18:50:19 +0900

    録画機能追加

--- WebCamera_DLL/Source.cpp
+++ WebCamera_DLL/Source.cpp
@@ -1,10 +1,14 @@
 #include "Source.h"
+#include "DebugLogWrapper.h"
 
 DLLExport void* getCamera(int device)
 {
     // ウィンドウを開く
     cv::namedWindow("web camera");
+	//普通にWebCameraから取ってくるとき
     return static_cast<void*>(new cv::VideoCapture(device));
+	//録画再生用	
+    //return static_cast<void*>(new cv::VideoCapture("capture.avi"));
 }
 
 DLLExport void setCameraProp(void* camera, int width, int height, int fps)
@@ -14,45 +18,54 @@ DLLExport void setCameraProp(void* camera, int width, int height, int fps)
 	vc->set(CV_CAP_PROP_FRAME_WIDTH, width);
 	vc->set(CV_CAP_PROP_FRAME_HEIGHT, height);
 	vc->set(CV_CAP_PROP_FPS, fps);
+	//録画用
+	//videoWriter = cv::VideoWriter("capture.avi", CV_FOURCC('X','V','I','D'), fps, cv::Size(width, height));
 }
 
 DLLExport void releaseCamera(void* camera)
 {
     // ウィンドウを閉じる
     cv::destroyWindow("web camera");
+	//videoWriter.release();
     auto vc = static_cast<cv::VideoCapture*>(camera);
     delete vc;
 }
 
-DLLExport void getCameraTexture(void* camera, unsigned char* data)
+DLLExport void getCameraTexture(void* camera, unsigned char* data, bool isRecord)
 {
     auto vc = static_cast<cv::VideoCapture*>(camera);
-    
-    // カメラ画の取得
-    cv::Mat img;
-    *vc >> img;
 
-	//std::cout << "img size:" << img.rows << " * " << img.cols << std::endl;
-    
-    // リサイズ
-	cv::Mat resized_img(img.rows * 0.5, img.cols * 0.5, img.type());
-    cv::resize(img, resized_img, resized_img.size(), cv::INTER_CUBIC);
-    
-    // 別ウィンドウの画を更新
-    cv::imshow("web camera", resized_img);
-    
-	//↓↓処理するのはこっちに戻ってやるのだからx反転色変換いらないのでは？？
-    // RGB --> ARGB 変換
- //   cv::Mat argb_img, flip_img;
- //   //cv::cvtColor(resized_img, argb_img, CV_RGB2BGRA);
- //   cv::cvtColor(img, argb_img, CV_RGB2BGRA);
- //   std::vector<cv::Mat> bgra;
- //   cv::split(argb_img, bgra);
- //   std::swap(bgra[0], bgra[3]);
- //   std::swap(bgra[1], bgra[2]);
-	////x軸反転
-	//cv::flip(argb_img, flip_img, 0);
- //   std::memcpy(data, flip_img.data, flip_img.total() * flip_img.elemSize());
+		// カメラ画の取得
+		cv::Mat img;
+		*vc >> img;
 
-    std::memcpy(data, img.data, img.total() * img.elemSize());
+		if(!img.empty())
+		{
+			//録画フラグがたっていたら録画もする
+			//if(isRecord) videoWriter << img;
+
+			//std::cout << "img size:" << img.rows << " * " << img.cols << std::endl;
+    
+			// リサイズ
+			cv::Mat resized_img(img.rows * 0.5, img.cols * 0.5, img.type());
+			cv::resize(img, resized_img, resized_img.size(), cv::INTER_CUBIC);
+    
+			// 別ウィンドウの画を更新
+			cv::imshow("web camera", resized_img);
+    
+			//↓↓処理するのはこっちに戻ってやるのだからx反転色変換いらないのでは？？
+			// RGB --> ARGB 変換
+		 //   cv::Mat argb_img, flip_img;
+		 //   //cv::cvtColor(resized_img, argb_img, CV_RGB2BGRA);
+		 //   cv::cvtColor(img, argb_img, CV_RGB2BGRA);
+		 //   std::vector<cv::Mat> bgra;
+		 //   cv::split(argb_img, bgra);
+		 //   std::swap(bgra[0], bgra[3]);
+		 //   std::swap(bgra[1], bgra[2]);
+			////x軸反転
+			//cv::flip(argb_img, flip_img, 0);
+		 //   std::memcpy(data, flip_img.data, flip_img.total() * flip_img.elemSize());
+
+			std::memcpy(data, img.data, img.total() * img.elemSize());
+		}
 }
--- WebCamera_DLL/Source.h
+++ WebCamera_DLL/Source.h
@@ -8,10 +8,12 @@
 
 //using namespace cv;
 
+//VideoWriter
+//cv::VideoWriter videoWriter;
 
 extern "C" {
    DLLExport void* getCamera(int device);
    DLLExport void setCameraProp(void* camera, int width, int height, int fps);
    DLLExport void releaseCamera(void* camera);
-   DLLExport void getCameraTexture(void* camera, unsigned char* data);
+   DLLExport void getCameraTexture(void* camera, unsigned char* data, bool isRecord);
 }
